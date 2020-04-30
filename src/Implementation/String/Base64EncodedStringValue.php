1fab71f2f227248bca3a7047f7320e5d27049b1c
Arne De Smedt <arnedesmdt@gmail.com>
2020-04-30 19:00:04 +0200

    add base64 encoded string

---
+++ src/Implementation/String/Base64EncodedStringValue.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\ValueObjects\Implementation\String;
+
+use RuntimeException;
+use function base64_decode;
+use function base64_encode;
+use function sprintf;
+
+abstract class Base64EncodedStringValue extends StringValue
+{
+    public static function fromPlainString(string $plainString) : self
+    {
+        return self::fromString(base64_encode($plainString));
+    }
+
+    public function toPlainString() : string
+    {
+        /** @var string|false $decoded */
+        $decoded = base64_decode($this->toString());
+
+        if ($decoded === false) {
+            throw new RuntimeException(
+                sprintf(
+                    'Could not base64 decode string %s',
+                    $this->toString()
+                )
+            );
+        }
+
+        return $decoded;
+    }
+}
