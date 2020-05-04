691304ef84f982c507f68d46297c0a75f0548987
Arne De Smedt <arnedesmdt@gmail.com>
2020-05-04 10:58:16 +0200

    add timestamp

---
+++ src/Implementation/Int/TimestampValue.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\ValueObjects\Implementation\Int;
+
+abstract class TimestampValue extends RangeValue
+{
+    public static function minimum() : int
+    {
+        return 0;
+    }
+}
