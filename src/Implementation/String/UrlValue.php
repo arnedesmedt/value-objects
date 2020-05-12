4f7473f7cff12be4cc88fafacd6915262df80a74
Arne De Smedt <arnedesmdt@gmail.com>
2020-05-12 14:06:38 +0200

    no idn to ascii for url only for host

--- src/Implementation/String/UrlValue.php
+++ src/Implementation/String/UrlValue.php
@@ -6,25 +6,16 @@ namespace ADS\ValueObjects\Implementation\String;
 
 use ADS\ValueObjects\Exception\UrlException;
 use function filter_var;
-use function idn_to_ascii;
 use const FILTER_VALIDATE_URL;
-use const IDNA_DEFAULT;
-use const INTL_IDNA_VARIANT_UTS46;
 
 abstract class UrlValue extends StringValue
 {
     protected function __construct(string $value)
     {
-        $url = idn_to_ascii($value, IDNA_DEFAULT, INTL_IDNA_VARIANT_UTS46);
-
-        if ($url === false) {
-            throw UrlException::noAsciiFormat($value, static::class);
-        }
-
-        if (! filter_var($url, FILTER_VALIDATE_URL)) {
+        if (! filter_var($value, FILTER_VALIDATE_URL)) {
             throw UrlException::noValidUrl($value, static::class);
         }
 
-        parent::__construct($url);
+        parent::__construct($value);
     }
 }
