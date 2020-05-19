a193fc810cda62fd55b7922217087a4dbd42242a
Arne De Smedt <arnedesmdt@gmail.com>
2020-05-19 09:00:42 +0200

    add isIdentifier

---
+++ src/IsIdentifier.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\ValueObjects;
+
+interface IsIdentifier
+{
+    /**
+     * @return class-string
+     */
+    public static function resource() : string;
+}
