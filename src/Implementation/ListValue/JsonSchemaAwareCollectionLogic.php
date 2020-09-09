f8683ca3f049447947ebea9b7c27d9472cc2f1dc
Arne De Smedt <arnedesmdt@gmail.com>
2020-09-09 11:17:20 +0200

    add typedetector in value objects

--- src/Implementation/ListValue/JsonSchemaAwareCollectionLogic.php
+++ src/Implementation/ListValue/JsonSchemaAwareCollectionLogic.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace ADS\ValueObjects\Implementation\ListValue;
 
+use ADS\ValueObjects\Implementation\TypeDetector;
+use EventEngine\JsonSchema\JsonSchema;
+use EventEngine\Schema\TypeSchema;
+
 /**
  * @method static string itemType()
  */
@@ -17,5 +21,25 @@ trait JsonSchemaAwareCollectionLogic
     private static function __itemType(): string
     {
         return static::itemType();
+    }
+
+    public static function __itemSchema(): TypeSchema
+    {
+        // phpcs:disable Squiz.NamingConventions.ValidVariableName.NotCamelCaps
+        if (self::$__itemSchema === null) {
+            $itemType = self::__itemType();
+
+            if ($itemType === null) {
+                return JsonSchema::any();
+            }
+
+            if (self::isScalarType($itemType)) {
+                return JsonSchema::schemaFromScalarPhpType($itemType, false);
+            }
+
+            self::$__itemSchema = TypeDetector::getTypeFromClass($itemType, self::__allowNestedSchema());
+        }
+
+        return self::$__itemSchema;
     }
 }
