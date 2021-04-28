386752c9d54e2dfceac046af539a013e0f6bee3c
Arne De Smedt <arnedesmdt@gmail.com>
2021-04-28 14:41:20 +0200

    no exception for domain specific value objects

--- src/Exception/UrlException.php
+++ src/Exception/UrlException.php
@@ -35,18 +35,4 @@ final class UrlException extends ValueObjectException
             )
         );
     }
-
-    /**
-     * @return static
-     */
-    public static function noValidUrlOrPath(string $value, string $class)
-    {
-        return new static(
-            sprintf(
-                '\'%s\' is not a valid url or path for value object \'%s\'.',
-                $value,
-                $class
-            )
-        );
-    }
 }
