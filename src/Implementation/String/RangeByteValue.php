13d813f05f9bd23bc167e327515815a5e60e20b4
Arne De Smedt <arnedesmdt@gmail.com>
2023-06-13 11:38:27 +0200

    Add range byte value

---
+++ src/Implementation/String/RangeByteValue.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\ValueObjects\Implementation\String;
+
+use ADS\ValueObjects\Exception\RangeException;
+
+use const PHP_FLOAT_MAX;
+use const PHP_FLOAT_MIN;
+
+abstract class RangeByteValue extends ByteValue
+{
+    protected function __construct(string $value)
+    {
+        $floatValue = (float) $value;
+
+        if (
+            (static::included() && ($floatValue > static::maximum() || $floatValue < static::minimum()))
+            || (! static::included() && ($floatValue >= static::maximum() || $floatValue <= static::minimum()))
+        ) {
+            throw RangeException::outsideRange(
+                $floatValue,
+                static::class,
+                static::minimum(),
+                static::maximum(),
+                static::included()
+            );
+        }
+
+        parent::__construct($value);
+    }
+
+    public static function minimum(): float
+    {
+        return PHP_FLOAT_MIN;
+    }
+
+    public static function maximum(): float
+    {
+        return PHP_FLOAT_MAX;
+    }
+
+    public static function included(): bool
+    {
+        return true;
+    }
+}
