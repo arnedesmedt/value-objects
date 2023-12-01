fe4070bcae873fcb1dedb968b835f8ec78cb4a70
Arne De Smedt <arnedesmdt@gmail.com>
2023-12-01 15:00:22 +0100

    Ignore rector rule

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -11,4 +12,12 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
     $rectorConfig->phpVersion(PhpVersion::PHP_81);
     $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
+
+    $rectorConfig->skip(
+        [
+            ReadOnlyPropertyRector::class => [
+                __DIR__ . '/src/Implementation/**/Exception/*.php',
+            ],
+        ],
+    );
 };
