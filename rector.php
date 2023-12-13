35843949ea8367f9d638e450b5ae7f55e760168c
Arne De Smedt <arnedesmdt@gmail.com>
2023-12-13 10:12:06 +0100

    Update to rector 8.2

--- rector.php
+++ rector.php
@@ -10,8 +10,8 @@ use Rector\Set\ValueObject\LevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     // get parameters
     $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
-    $rectorConfig->phpVersion(PhpVersion::PHP_81);
-    $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
+    $rectorConfig->phpVersion(PhpVersion::PHP_82);
+    $rectorConfig->import(LevelSetList::UP_TO_PHP_82);
 
     $rectorConfig->skip(
         [
