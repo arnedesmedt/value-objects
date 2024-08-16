b0239614278f7083d8390002720ca1667f90e993
Arne De Smedt <arnedesmdt@gmail.com>
2024-08-16 15:00:39 +0200

    Use secret encoded string as an interface

--- src/Implementation/String/SecretEncodedStringValue.php
+++ src/Implementation/String/SecretEncodedStringValue.php
@@ -4,27 +4,8 @@ declare(strict_types=1);
 
 namespace ADS\ValueObjects\Implementation\String;
 
-use ADS\ValueObjects\Service\EncryptDecryptService;
+use ADS\ValueObjects\SecretEncodedStringValue as SecretEncodedStringInterface;
 
-abstract class SecretEncodedStringValue extends StringValue
+abstract class SecretEncodedStringValue extends StringValue implements SecretEncodedStringInterface
 {
-    final protected function __construct(string $value, protected string $plainValue)
-    {
-        parent::__construct($value);
-    }
-
-    public static function fromString(string $value): static
-    {
-        return new static(EncryptDecryptService::encrypt($value), EncryptDecryptService::decrypt($value));
-    }
-
-    public function toPlainString(): string
-    {
-        return $this->plainValue;
-    }
-
-    public static function example(): static
-    {
-        return static::fromString('This is a secret message');
-    }
 }
---
+++ src/SecretEncodedStringValue.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\ValueObjects;
+
+interface SecretEncodedStringValue extends StringValue
+{
+}
